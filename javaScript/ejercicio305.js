4f97ff8fd9622dc5f3391aaba1bb426c8eb1ecd2
Jorge aml <martjorge8@gmail.com>
2020-06-17 17:38:45 -0600

    Update ejercicio305.js

--- javaScript/ejercicio305.js
+++ javaScript/ejercicio305.js
@@ -14,14 +14,14 @@ function ejercicio2 () {
 	console.log(formula);
 }
 ejercicio2();
-//Nivel3
+//Nivel 3
 //115 de 82%
 function ejercicio3 (a, b){
 	var formula = (a * porcentaje) / b;
 	console.log(formula);
 }
 ejercicio3(115, 82);
-//nivel4
+//nivel 4
 //420 de 36%
 function ejercicio4 (a, b) {
 	if (a >= 0 && a <= 9999 && b >= 0 && b <= 9999) {
@@ -31,7 +31,7 @@ function ejercicio4 (a, b) {
 	return 0;
 }
 ejercicio4(420, 36);
-//nivel5
+//nivel 5
 //850 de 72%
 function ejercicio5 (a, b) {
 	numero = a;
@@ -45,3 +45,36 @@ function ejercicio5 (a, b) {
 		}
 }
 ejercicio5(123000, -12312);
+//nivel 6
+//16 el 1/4%;
+function ejercicio7 (a, b, c) {
+	numero = a;
+	numerador = b;
+	denominador = c;
+	const condicion = a >= 0 & a <= 9999 & b >= 0 & b <= 9999;
+	if (condicion) {
+		var formula = (numero * porcentaje) / (numerador / denominador);
+			console.log(formula);
+	} else {
+		console.log(false);
+	}
+	
+}
+ejercicio7(16, 1, 4);
+
+//Nivel 7
+//70 el 3 1/2%.
+function ejercicio11 (a, b, c, d) {
+	numero = a;
+	entero = b;
+	numerador = c;
+	denominador = d;
+	const condicion = a >= 0 & a <= 9999 & b >= 0 & b <= 9999;
+		if (condicion) {
+			var formula = (numero * porcentaje) / (((entero * denominador) + numerador)/denominador);
+				console.log(formula);
+		} else {
+			console.log(false);
+		}
+}
+ejercicio11(70, 3, 1, 2);
