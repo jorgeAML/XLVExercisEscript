abc3a29746e81a9767c0021c2f77e853831114a6
Jorge aml <martjorge8@gmail.com>
2020-06-17 16:45:21 -0600

    Create ejercicio305.js

---
+++ javaScript/ejercicio305.js
@@ -0,0 +1,16 @@
+//Programa que calcule el tanto por ciento de el
+//De que numero es 35 el 5%.
+function ejercicio1 () {
+	var formula = (35 * 100) / 5;
+	console.log(formula);
+}
+ejercicio1();
+
+//60 el 90%
+const porcentaje = 100;
+function ejercicio2 () {
+	
+	var formula = (60*porcentaje)/90;
+	console.log(formula);
+}
+ejercicio2();
