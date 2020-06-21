ad97a961088a9e293f22d61e27bcabd8bf205eb0
Jorge aml <martjorge8@gmail.com>
2020-06-21 10:19:38 -0600

    Update ejercicio307.js

--- javaScript/ejercicio307.js
+++ javaScript/ejercicio307.js
@@ -1,17 +1,38 @@
 //Programa para el ejercicio 307
-
+console.log("*************************************************");
 console.log("El siguiente programa calcula el primer numero que es su 100% y debemos averiguar el segundo");
 function guest (a, b) {
 	const one = 100;
 		if (a > 0 && a <= 9999 && b > 0 && b <= 9999) {
 			let formula = (b * one) / a;
-				console.log("Que % de " + a + " es " + b);
+				console.log("Que % de " + a + " es " + b + ".");
 					console.log(formula);
 		}
 }
-console.log("Calculating...");
+console.log("Calculando...");
 function respond () {
 	guest(8400, 2940);
+	console.log("----------------");
 }
 
 var t = setTimeout(respond, 1000);
+
+/*CONSTANTES*/
+const oneHundred = 100;
+/*CONSTANTES*/
+
+function ej1 (a, b) {
+	if (a > 0 && a <= 9999 && b > 0 && b <= 9999) {
+		let formula = (b * oneHundred) / a ;
+			console.log("Que % de " + a + " es " + b + ".");
+				console.log(formula);
+		
+	}
+}
+function respon1 () {
+	ej1(860, 129);
+	console.log("----------------");
+}
+var t1 = setTimeout(respon1, 1500);
+
+
