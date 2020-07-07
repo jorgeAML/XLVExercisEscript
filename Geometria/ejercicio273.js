0a1da1d1a4e058f28625a1e235f600d65603cafd
Jorge aml <martjorge8@gmail.com>
2020-07-06 21:24:47 -0600

    Update ejercicio273.js

--- Geometria/ejercicio273.js
+++ Geometria/ejercicio273.js
@@ -1,5 +1,13 @@
 //Ejercicio 273
+/*
+	EL PARAMETRO 'a' SERA SIEMPRE LA BASE DEL TRIANGULO
+	EL PARAMETRO 'b' SERA SIEMPRE LA ALTURA DEL TRIANGULO
+*/
 const formulaTriangulo = 2;
+const cienCentimetros = 100;
+const milCentimetros = 1000;
+const diezCentimetros = 10;
+
 function ejercicio1 (a, b) {
 	if (a > 0 && a < 9999 || b > 0 && b < 9999) {
 		let formula = (a * b) / formulaTriangulo;
@@ -13,5 +21,27 @@ function ejercicio1 (a, b) {
 function respon1 () {
 	ejercicio1(10, 42);
 	console.log("-----------------------");
-}
+};
 var ej1 = setTimeout(respon1, 500);
+
+function ejercicio2 (a, b, c) {
+	if (a > 0 && a < 9999 || b > 0 && b < 9999) {
+	/* 
+		CONVERTIR TODO A METROS
+	*/
+		let aa = a / cienCentimetros;
+		let bb = b / milCentimetros;
+		let cc = c / diezCentimetros;
+		let formula = ((aa + bb) * cc) / formulaTriangulo;
+		let des = "La base de un triangulo es " + aa + "cms" + " " + bb + "mms" + " y la altura " + cc + "dms" + " Hallar el area en metros cuadrados";
+			console.log(des);
+			console.log(formula);
+	} else {
+		console.log(false);
+	}
+};
+function respond2 () {
+	ejercicio2 (8, 6, 0.84);
+	console.log("------------------------");
+};
+var ej2 = setTimeout(respond2, 1000);
