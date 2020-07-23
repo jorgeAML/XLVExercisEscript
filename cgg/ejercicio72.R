c9410987ee7388cf7dc724022e7900ac4273231b
Jorge aml <martjorge8@gmail.com>
2020-07-22 21:58:47 -0600

    Update ejercicio72.R

--- cgg/ejercicio72.R
+++ cgg/ejercicio72.R
@@ -5,6 +5,17 @@ dos <- 5 ^ 4
 print(dos)
 tres <- 7 ^ 3
 print(tres)
+
 # Hallar el valor de:
 cuatro <- 2^0 * 2
 print(cuatro)
+a1 <- 3^0 * 5^4
+print(a1)
+# [1] 625
+a2 <- 4^2 * 3^2
+print(a2)
+# [1] 144
+a3 <- 5^0 * 3^7 * 6^0
+print(a3)
+# [1] 2187
+
