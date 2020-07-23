2e242ad23a0ea42100cb5f683871cd3aa7237770
Jorge aml <martjorge8@gmail.com>
2020-07-22 22:15:06 -0600

    Update ejercicio72.R

--- cgg/ejercicio72.R
+++ cgg/ejercicio72.R
@@ -1,4 +1,5 @@
-# this is an example basic
+# this is an basic example using R
+# just a control how I know R for use a Machine Learning project.
 uno <- 6 ^ 3
 print(uno)
 dos <- 5 ^ 4
@@ -18,4 +19,30 @@ print(a2)
 a3 <- 5^0 * 3^7 * 6^0
 print(a3)
 # [1] 2187
+a4 <- 2^0 * 3^0 * 4^0 * 5^0
+print(a4)
+[1] 1
+a5 <- 3^3 * 4^2 * 5^4
+print(a5)
+#[1] 270000
+polyroot(c(-3,-5,2))
+# [1] -0.5+0i  3.0-0i
+a6 <- 2^10 * 10^2 * 8^0
+print(a6)
+# [1] 102400
+a7 <- 6^2 * 9^0 * 2^10
+print(a7)
+# [1] 36864
+a8 <- 3^0 / (2^2 * 3^2)
+print(a8)
+#[1] 0.02777778
+a9 <- 5^3 / 3^0
+print(a9)
+#[1] 125
+a10 <- 3^2 * 3^0 / 9
+print(a10)
+# [1] 1
+a11 <- (2^4 * 5^2) / (5^0 * 4^2)
+print(a11)
+# [1] 25
 
