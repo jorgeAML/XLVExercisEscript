f32b52e875087789e3dcc4b2444057c6d818581a
Jorge aml <martjorge8@gmail.com>
2020-07-27 22:03:59 -0600

    Update ejercicio60.R

--- cgg/ejercicio60.R
+++ cgg/ejercicio60.R
@@ -16,5 +16,21 @@ ejercicio1(750, 1250)
 # Introduce su diferencia primero (VARIABLE: a) y luego la sumatoria (VARIABLE: b)
 ejercicio1(9856, 45678)
 
+# La siguiente funcion encuentra el triplo de la suma de dos numeros y el duplo de la diferencia de los dos numeros. El siguiente ejemplo acalara lo anterior.
 # El triplo de la suma de dos numeros es 1350 y el duplo de su diferencia es 700. Hallar los numeros:
+ejercicio3 <- function(a,b) {
+  if (a != 0 & b != 0) {
+    a1 <- a/3 + b/2
+    a2 <- a1 / 2
+    a3 <- a2 * 3
+    a4 <- a - a3
+    a5 <- a4 / 3
+    vector <- c(a2, a5)
+    sprintf("Respuesta: %i", vector)
+    } else {
+    print("ERROR")
+    }
+  }
+ejercicio3(1350, 700)
+
 
