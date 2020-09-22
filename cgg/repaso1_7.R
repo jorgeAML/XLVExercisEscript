f5b575f0359020e199aa8c60f0ae9798cb69bc35
Jorge aml <martjorge8@gmail.com>
2020-09-22 11:54:00 -0600

    Update repaso1_7.R
    ejercicio 1.7 parte 2.a

--- cgg/repaso1_7.R
+++ cgg/repaso1_7.R
@@ -9,3 +9,11 @@ if (a1 <= a2) {
   } else {
     print("El segudo es mayor")
 }
+# Encontrar si es menor, mayor o igual
+b1 <- (-0.1)
+b2 <- (-0.01)
+if ( b1 < b2 ) {
+  sprintf ("El primero es menor: %f", b1)
+  } else {
+  print ("El segundo es mayor")
+  }
