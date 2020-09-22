48bcafbb95e6e4038f1cc6bb42ccf2c6e4cbe99a
Jorge aml <martjorge8@gmail.com>
2020-09-22 11:26:40 -0600

    Create repaso1_7.R

---
+++ cgg/repaso1_7.R
@@ -0,0 +1,11 @@
+# Formule cada una de las siguientes expresiones
+# como un número racional con el numerador postivo
+# más pequeño posible
+a1 <- 2/3
+a2 <- (-5/8)
+
+if (a1 <= a2) {
+  sprintf("El primero es menor: %i", a1)
+  } else {
+    print("El segudo es mayor")
+}
