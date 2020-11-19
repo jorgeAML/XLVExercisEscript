3ad2b1a208ad7769e16c3efc59ccb204d584ee7f
Jorge aml <martjorge8@gmail.com>
2020-11-19 17:28:12 -0600

    Update FindingRunwayLength.java

--- Chapter2-Elementary-Programming/FindingRunwayLength.java
+++ Chapter2-Elementary-Programming/FindingRunwayLength.java
@@ -1,4 +1,10 @@
-/**Physics: Finding runway length */
+/**Physics: Finding runway length 
+ * Given the airplane acceleration 'a' and take off speed 'v' you can compute the minimum runway
+ * length needed for an airplane to take-off using  the following formula:
+ * length = v^2 /2a
+ * Write a program that prompts the user to enter 'v' in meters/second (m/s) and the acceleration 'a'
+ * in meter/seconds squared (m/s^2), and displays the minimum runway length.
+*/
 public class FindingRunwayLength {
     public static void main(String[] args) {
         
