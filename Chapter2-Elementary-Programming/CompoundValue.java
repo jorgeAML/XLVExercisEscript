35f383acb4ca152909da811dc4a7c370935dc478
Jorge aml <martjorge8@gmail.com>
2020-11-19 18:07:13 -0600

    Create CompoundValue.java

---
+++ Chapter2-Elementary-Programming/CompoundValue.java
@@ -0,0 +1,19 @@
+/**Financial application: Compound Value 
+ * Suppose you save $100 each month into a savings account with annual interest rate 5%. 
+ * Thus the monthly interest rate is 0.05/12 = 0.00417. After the first month, the value 
+ * in the account becomes:
+ * 100 * (1 + 0.00417) = 100.417
+ * after the second month, the value in the account becomes 
+ * (100 + 100.417) * (1 + 0.00417) = 201.252
+ * After the third month, the value in the account becomes
+ * (100 + 202.252) * (1 + 0.00417) = 302.507
+ * and so on.
+ * Write a program that prompts the user to enter a monthly saving amount and
+ * displays the account value after six month. (In exercise 5.30 you will use a loop to simply
+ * the code and display the account value for any month).
+*/
+public class CompoundValue {
+    public static void main(String[] args) {
+        
+    }
+}
