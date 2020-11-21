2b9749157ad8c44fcba19416a62330b3759e760f
Jorge aml <martjorge8@gmail.com>
2020-11-20 20:32:39 -0600

    Create PhysicsAcceleration.java

---
+++ Chapter2-Elementary-Programming/PhysicsAcceleration.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**Physics Acceleration
+ * Wrtie a program that prompts the user to enter the starting velocity V0 in meters/seconds
+ * the ending velocity V1 in meters/seconds and the time span T in seconds,
+ * diplays the average acceleration.
+ * a = (V1 - V0) / T
+ */
+public class PhysicsAcceleration {
+    public static void main(String[] args) {
+        //Scanner input
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your velocity in point One, then in point Zero and the Time to get the average of acceleration: ");
+        //Enter the velocity 1
+        double velocityOne = input.nextDouble();
+        //enter the velocity 0
+        double velocityZero = input.nextDouble();
+        //enter the time
+        double time = input.nextDouble();
+        //make the formula
+        double averageAcceleration = (velocityOne - velocityZero) / time;
+        //output the average acceleration
+        System.out.println("The average of acceleration is: " + averageAcceleration);
+
+    }
+}
