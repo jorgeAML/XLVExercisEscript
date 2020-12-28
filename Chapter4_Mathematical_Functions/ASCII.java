4c676bb2e407b6f3bfad81c65035934d57bd1ef7
JorgeAML <martjorge8@gmail.com>
2020-12-28 04:27:20 -0600

    ASCII exercises for decimal codes

--- Chapter4_Mathematical_Functions/ASCII.java
+++ Chapter4_Mathematical_Functions/ASCII.java
@@ -7,5 +7,13 @@ public class ASCII {
         int lettera = (byte)'a';
         int letterb = (byte)'b';
         System.out.println("ASCII Code: \n" + one + "\t" + letterA + "\n" + letterB + "\t" + lettera + "\n" + letterb);
+        /**USE PRINT STATEMENTS TO FFIND OUT THE CHARACTERS FOR THE DECIMAL CODES 40 59 79 85 AND 90 */
+        char decimal40 = (char)40;
+        char decimal59 = (char)59;
+        char decimal79 = (char)79;
+        char decimal85 = (char)85;
+        char decimal90 = (char)90;
+        System.out.println("---------------------");
+        System.out.println(decimal40 + "\t" + decimal59 + "\n" + decimal79 + "\t" + decimal85 + "\n" + decimal90);
     }
 }
