a0cbf6ec493a0e115241efc658075360500893d4
JorgeAML <martjorge8@gmail.com>
2021-07-29 20:31:06 -0600

    2.13 Compound Value

--- Chapter2-Elementary-Programming/CompoundValue.java
+++ Chapter2-Elementary-Programming/CompoundValue.java
@@ -1,18 +1,5 @@
 import java.util.Scanner;
-/**Financial application: Compound Value 
- * Suppose you save $100 each month into a savings account with annual interest rate 5%. 
- * Thus the monthly interest rate is 0.05/12 = 0.00417. After the first month, the value 
- * in the account becomes:
- * 100 * (1 + 0.00417) = 100.417
- * after the second month, the value in the account becomes 
- * (100 + 100.417) * (1 + 0.00417) = 201.252
- * After the third month, the value in the account becomes
- * (100 + 202.252) * (1 + 0.00417) = 302.507
- * and so on.
- * Write a program that prompts the user to enter a monthly saving amount and
- * displays the account value after six month. (In exercise 5.30 you will use a loop to simply
- * the code and display the account value for any month).
-*/
+
 public class CompoundValue {
     public static void main(String[] args) {
         //we cannot use a loop in this exercise.
@@ -29,7 +16,13 @@ public class CompoundValue {
         double accountBecomes = amount * FORMULAONE;
         //After the second MONTH
         double accountSecondMonth = (amount + accountBecomes) * (FORMULAONE);
+        double accountThirdMonth =  (amount + accountSecondMonth) * (FORMULAONE);
+        double accountFourthMonth = (amount + accountThirdMonth) * (FORMULAONE);
+        double accountFifthMonth = (amount + accountFourthMonth) * (FORMULAONE);
+        double accountSixMonth = (amount + accountFifthMonth) * (FORMULAONE);
         //output his bank account
-        System.out.println("After the first month, the account value is: " + accountBecomes + "\n" + "Second Month: " + accountSecondMonth);
+        System.out.println("After the first month, the account value is: " + accountBecomes + "\n" + "Second Month: " + accountSecondMonth + "\n" + "Third Month: " + accountThirdMonth);
+        System.out.println("Fourth Month: " + accountFourthMonth + "\n" + "Fifth Month: " + accountFifthMonth);
+        System.out.println("Six Month: " + accountSixMonth);
     }
 }
--- Chapter2-Elementary-Programming/readme.md
+++ Chapter2-Elementary-Programming/readme.md
@@ -150,3 +150,26 @@ Write a program that prompts the user to enter **v** in meters/second (m/s) and 
 [Find the answer here](https://github.com/jorgeAML/XLVExercisEscript/blob/master/Chapter2-Elementary-Programming/FindingRunwayLength.java)
 
 ---
+### 2.13 Financial Application: compound value
+Suppose you save **$100** each month into a savings account with the annual interest rate 5%. Thus, the monthly interest rate is 0.05/12 = 0.00417. After the first month, the value in the account becomes:
+
+> 100 * (1 + 0.00417) = 100.417
+
+After the second month, the value in the account becomes:
+
+> (100 + 100.417) * (1 + 0.00417) = 201.252
+
+After the third month, the value in the account becomes: 
+
+> (100 + 201.252) * (1 + 0.00417) = 302.507
+
+and so on. Write a program that prompts the user to enter a monthly saving amount and displays the account value after the sixth month. (In exercise 5.30, you will use a loop to simply the code and display the account value for any month).
+
+> Enter the monthly savoing amount: 100
+>
+> After the six month, the account value is $608.81
+
+[Find the answer here](https://github.com/jorgeAML/XLVExercisEscript/blob/master/Chapter2-Elementary-Programming/CompoundValue.java)
+
+---
+
