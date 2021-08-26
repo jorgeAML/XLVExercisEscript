a852304d14c9e0a1e634a7f398a0f26816a07751
jorgeAML <martjorge8@gmail.com>
2021-08-26 12:49:43 -0600

    2.18 Print a table

---
+++ Chapter2-Elementary-Programming/PrintATable.java
@@ -0,0 +1,16 @@
+public class PrintATable {
+    public static void main(String ... args) {
+        double one = Math.pow(1,2);
+        double two = Math.pow(2,3);
+        double three = Math.pow(3,4);
+        double four = Math.pow(4,5);
+        double five = Math.pow(5,6);
+
+        System.out.println("a" + " " + "b" + " " +  "pow(a,b)");
+        System.out.println("1" + " " + "2" + " " + (byte)one);
+        System.out.println("2" + " " + "3" + " " + (int)two);
+        System.out.println("3" + " " + "4" + " " + (int)three);
+        System.out.println("4" + " " + "5" + " " + (int)four);
+        System.out.println("5" + " " + "6" + " " + (int)five);
+    }
+}
