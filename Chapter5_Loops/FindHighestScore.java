88b29f221bc738acc96cbb39c78d5f5a1ca4573c
jorgeAML <martjorge8@gmail.com>
2021-09-26 15:19:34 -0600

    Find highest score

--- Chapter5_Loops/FindHighestScore.java
+++ Chapter5_Loops/FindHighestScore.java
@@ -3,19 +3,26 @@ import java.util.Scanner;
 public class FindHighestScore {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int number, max;
-        number = input.nextInt();
-        max = number;
-        //controller
-        int count = input.nextInt();
-        do {
-            System.out.println("Enter the score of students: ");
-            number = input.nextInt();
-            if(number > max)
-                max = number;
-        } while(number != 0); 
-        System.out.println("Max score is: " + max);
-        System.out.println("number is: " + number);
-        
+
+        System.out.println("How many students do you have?: ");
+        int numberOfStudents = input.nextInt();
+                                                                                     
+        int max = 0;
+        String maxName = "";
+
+        int i = 0;
+        while (i < numberOfStudents ) {
+            System.out.println("Enter their name and score (name score): (" + (i + 1) + "): ");
+            String name = input.next();
+            int score = input.nextInt();
+            i++;
+            if (score > max) {
+                max = score;
+                maxName = name;
+            }
+
+        }
+        System.out.println("Student with the higher score: " + maxName);
+        System.out.println("The highest score: " + max); 
     }
 }
