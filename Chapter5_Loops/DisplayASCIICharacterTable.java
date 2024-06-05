794656d58b2d6346dab26d038dbdcec623fd073f
Jorge aml <martjorge8@gmail.com>
2024-06-05 10:15:33 -0600

    Exercise 5.14

--- Chapter5_Loops/DisplayASCIICharacterTable.java
+++ Chapter5_Loops/DisplayASCIICharacterTable.java
@@ -2,11 +2,26 @@ public class DisplayASCIICharacterTable {
 	public static void main(String[] args) {
 		//from 33 to 126
 		
-		char begin = 32;
-		char end = 125;
-		while(begin <= end ) {
-			begin += 1;
-			System.out.println("The symbols are: " + begin);
+		char begin = 33;
+		char end = 126;
+		System.out.println("      TABLA ASCII");
+		//Filas
+		System.out.println("    ");
+		for (int i = 1; i <= 10; i++) {
+			System.out.print("  " + i);
 		}
+		System.out.println("\n---------------------------------");
+		
+		int count = 0;
+		for (int i = begin; i <= end; i++) {
+			count++; //El incremento
+			//Muestre 10 caracteres por linea
+			if(count % 10 == 0) {
+				System.out.println((char)i);
+			} else {
+				System.out.print((char)i + "  ");
+			}
+		}
+		System.out.println();
 	}
 }
